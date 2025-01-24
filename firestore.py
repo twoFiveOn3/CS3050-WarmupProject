83872dca3e989665223aed32e8f3ce412d42630f
Conor <conordmcdevitt@gmail.com>
2025-01-24 15:47:10 -0500

    csv to json for car data

--- firestore.py
+++ firestore.py
@@ -2,11 +2,15 @@ import firebase_admin
 import os
 from firebase_admin import credentials
 from firebase_admin import firestore
+import json 
 
 cred = credentials.Certificate(f"{os.getcwd()}/firebase_secrets.json")
 app = firebase_admin.initialize_app(cred)
 
+with open('Cars.csv')
 db = firestore.client()
 
-doc_ref = db.collection("users").document("alovelace")
+doc_ref = db.collection("users").document("car_data")
 doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+
+
