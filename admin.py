0079c70609fccc34ab051be1817852af4feb882b
zach-vanzura <zvanzura@uvm.edu>
2025-01-24 15:59:24 -0500

    added admin.py

--- admin.py
+++ admin.py
@@ -2,13 +2,11 @@ import firebase_admin
 import os
 from firebase_admin import credentials
 from firebase_admin import firestore
-import json 
 
 cred = credentials.Certificate(f"{os.getcwd()}/firebase_secrets.json")
 app = firebase_admin.initialize_app(cred)
 
-with open('Cars.csv'):
-    db = firestore.client()
+db = firestore.client()
 
-doc_ref = db.collection("users").document("car_data")
+doc_ref = db.collection("users").document("alovelace")
 doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
