49f608571498b64efcdc526491c58ab97ff242e1
zach-vanzura <zvanzura@uvm.edu>
2025-01-28 10:50:06 -0500

    began working on interface

--- admin.py
+++ admin.py
@@ -1,4 +1,5 @@
 import firebase_admin
+import json
 import os
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -6,7 +7,16 @@ from firebase_admin import firestore
 cred = credentials.Certificate(f"{os.getcwd()}/firebase_secrets.json")
 app = firebase_admin.initialize_app(cred)
 
+cars = {}
+
+with open('cars.json') as file:
+    data = json.load(file)
+    for car in data:
+
+
+print(type(data))
+
 db = firestore.client()
 
-doc_ref = db.collection("users").document("alovelace")
-doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+doc_ref = db.collection("Warm Up Project - Cars").document("Car")
+doc_ref.set(data)
