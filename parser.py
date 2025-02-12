ef7f20f98e1287462d5693fe9bc86fee7b68d76e
Eli Malloy <elimalloy@icloud.com>
2025-02-12 16:04:28 -0500

    made the parser better

--- parser.py
+++ parser.py
@@ -1,33 +1,39 @@
 from pyparsing import *
-from query import make_query
 
-def parse(querystring: str):
+def parse(query_string: str):
     # remove newline from whitespace characters, because newline terminates queries
     ParserElement.set_default_whitespace_chars(' \t')
-
-    # fieldname is a string of letters identifying a field within the database
-    fieldname = Word(alphas)
     
-    # The operator set is small enough that they can be written explicitely
+	# The operator set is small enough that they can be written explicitely
     # Every operator has a negative, which paired with 'or' and the demorgan laws would allow any
     # logical expression to be representable, but oh well.
     op = (  Keyword("==") | Keyword("!=") | Keyword("is") | 
             Keyword(">") | Keyword(">=") | 
             Keyword("<") | Keyword("<="))
 
-    # all fields in the db are alphabetic string or integer valued
-    value = Word(alphas) | Word(nums)
+    numeric_field      = CaselessKeyword("price") | CaselessKeyword("msrp") | CaselessKeyword("horsepower")
+    alphabetic_field   = CaselessKeyword("make") | CaselessKeyword("model")
+    boolean_field      = CaselessKeyword("awd")
+	
+    numeric_value      = Combine(Word(nums) + Optional("." + Word(nums)))
+    alphabetic_value   = Word(alphas)
+    boolean_value      = CaselessKeyword("true") | CaselessKeyword("false")
 
-    querytriplet = fieldname + op + value
-    additionalcondition = Keyword("and") + querytriplet
+    numeric_triplet    = numeric_field + op + numeric_value
+    alphabetic_triplet = alphabetic_field + op + alphabetic_value
+    boolean_triplet    = boolean_field + op + boolean_value
+
+    query_triplet      = numeric_triplet | alphabetic_triplet | boolean_triplet
+
+    additional_condition = Suppress(CaselessKeyword("and")) + query_triplet
     
     # we have one (fieldname op value) triplet, then 0 or more (and fieldname op value) quads
     # restricting the ql to a single 'and' is lazy and we're not doing it. I will fix the query function if that's a problem.
-    query = querytriplet + ZeroOrMore(additionalcondition)
-    parsedquery = query.parseString(querystring)
+    query = query_triplet + ZeroOrMore(additional_condition)
+    parsed_query = query.parseString(query_string)
     
     #TODO: return parsed query as a list of lists
-    print(parsedquery)
+    print(parsed_query)
 
     
 
@@ -40,10 +46,10 @@ parse("make is toyota")
 parse("name == Mini Cooper")
 #make_query([["msrp", ">", 30000]])
 parsed_str = parse("msrp > 30000")
-make_query(parsed_str)
+#make_query(parsed_str)
 
 #make_query([["msrp", ">", 30000], ["horsepower", ">", 300]])
 
 #TODO: parser returns ['msrp', '>', '30000', 'and', 'horsepower', '>', '300'] instead of [['msrp', '>', '30000'], ['horsepower', '>', '300']]
 parsed_str = parse("msrp > 30000 and horsepower > 300")
-make_query(parsed_str)
+#make_query(parsed_str)
