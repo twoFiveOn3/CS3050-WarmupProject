1348dfc645c1c55cdac678048ecac3b6a2f028af
Conor <conordmcdevitt@gmail.com>
2025-02-13 13:30:43 -0500

    all queries working?

--- admin.py
+++ admin.py
@@ -5,9 +5,9 @@ import firebase_admin
 
 from auth import auth
 from firebase_admin import firestore
-from parser import parse
-from query_test import make_query
-from car import Car
+
+
+from auth import auth
 
 
 def main():
@@ -28,29 +28,8 @@ def main():
     drop_collection()
     create_docs(user_input)
 
-    usr_query = ''
-    print_help_menu()
-    # begin while loop polling for using input
+    
 
-    while usr_query.lower() != 'quit':
-        usr_query = input('> ')
-        if usr_query.lower() == 'help':
-            print_help_menu()
-            continue  # restart while loop
-        if usr_query.lower() == 'quit':
-            exit()
-        # otherwise call parse
-        try:
-            print("hello")
-            query_elems = parse(usr_query)
-            
-            print("From admin", query_elems)
-            make_query(query_elems)
-        except Exception as e:
-            print("Error: ", e, "try again, for help making a query, type 'help'")
-            continue
-        # print return data
-        usr_query = input('> ')
 
 
 def create_docs(json_file: str):
@@ -74,6 +53,7 @@ def drop_collection():
     docs = db.collection("cars").stream()
     batch = db.batch()
 
+   
     print("Dropping collection...")
     for doc in docs:
         batch.delete(doc.reference)
@@ -81,14 +61,6 @@ def drop_collection():
     batch.commit()
 
 
-def print_help_menu():
-    print("Query should be in the form: <field> <operator> <delimiter>\n"
-          "The language supports the following operators: \n == \n >= \n <= \n < \n > \n "
-          "The language supports the following fields: \n Make \n Model \n Price \n MSRP \n Horsepower \n AWD (boolean)"
-          "To make a compound query, do so in the form: <field 1> <operator 1> <delimiter 1> 'and' <field 2> <operator 2> <delimiter 2 \n"
-          "To search for a field longer than one word, wrap the field with double quotes \n"
-          "For help: type 'help' to see this menu again \n"
-          "To exit: type 'quit' ")
 
 main()
 
--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 from pyparsing import *
+from query_test import make_query
+from auth import auth
 
 def parse(query_string: str):
     # remove newline from whitespace characters, because newline terminates queries
@@ -12,7 +14,7 @@ def parse(query_string: str):
             Keyword("<") | Keyword("<="))
 
     numeric_field      = CaselessKeyword("price") | CaselessKeyword("msrp") | CaselessKeyword("horsepower")
-    alphabetic_field   = CaselessKeyword("make") | CaselessKeyword("model")
+    alphabetic_field   = CaselessKeyword("make") | CaselessKeyword("model") | CaselessKeyword("name")
     boolean_field      = CaselessKeyword("awd")
 	
     multiword = Suppress("\"") + Combine(Word(alphas) + ZeroOrMore(" " + Word(alphas))) + Suppress("\"")
@@ -36,24 +38,57 @@ def parse(query_string: str):
     
     #TODO: return parsed query as a list of lists
     rtn = [parsed_query[i:i + 3] for i in range(0, len(parsed_query), 3)]
-    print(rtn)
+    print(rtn, "THIS IS RTN IN PARSER")
     return rtn
 
+def interface():
+    auth()
+    usr_query = ''
+    print_help_menu()
+    # begin while loop polling for using input
+
+    #this works 
+    #make_query([["msrp", ">", 30000]])
+    while usr_query.lower() != 'quit':
+        usr_query = input('> ')
+        if usr_query.lower() == 'help':
+            print_help_menu()
+            continue  # restart while loop
+        if usr_query.lower() == 'quit':
+            exit()
+        # otherwise call parse
+        try:
+            query_elems = parse(usr_query)
+            
+            make_query(query_elems)
+        except Exception as e:
+            print("Error: ", e, "try again, for help making a query, type 'help'")
+            continue
+
+def print_help_menu():
+    print("Query should be in the form: <field> <operator> <delimiter>\n"
+          "The language supports the following operators: \n == \n >= \n <= \n < \n > \n "
+          "The language supports the following fields: \n Make \n Model \n Price \n MSRP \n Horsepower \n AWD (boolean)"
+          "To make a compound query, do so in the form: <field 1> <operator 1> <delimiter 1> 'and' <field 2> <operator 2> <delimiter 2 \n"
+          "To search for a field longer than one word, wrap the field with double quotes \n"
+          "For help: type 'help' to see this menu again \n"
+          "To exit: type 'quit' ")
     
+interface()
 
 
 #for testing 
-parse("make is toyota")
+#parse("make is toyota")
 
 
 #TODO: parser gets rid of "Cooper" in "Mini Cooper"
-parse("make == \"Mini Cooper\"")
+#parse("make == \"Mini Cooper\"")
 #make_query([["msrp", ">", 30000]])
-parsed_str = parse("msrp > 30000")
+#parsed_str = parse("msrp > 30000")
 #make_query(parsed_str)
 
 #make_query([["msrp", ">", 30000], ["horsepower", ">", 300]])
 
 #TODO: parser returns ['msrp', '>', '30000', 'and', 'horsepower', '>', '300'] instead of [['msrp', '>', '30000'], ['horsepower', '>', '300']]
-parsed_str = parse("msrp > 30000 and horsepower > 300")
+#parsed_str = parse("msrp > 30000 and horsepower > 300")
 #make_query(parsed_str)
